e67f6676442c19522768426f0632cf1970098bd0
itezcan <itezcan@uos.de>
2022-11-08 21:09:03 +0100

    some more stuff that may or may not work

--- Week 2/Layer.py
+++ Week 2/Layer.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-relu = np.vectorize(lambda x: x if x > 0 else 0)
+relu = np.vectorize(lambda x: 0 if x < 0 else x)
+d_relu = np.vectorize(lambda x: 0 if (x < 0) else 1)  # what if x = 0?
 
 
 class Layer:
@@ -8,22 +9,30 @@ class Layer:
     def __init__(self, n_units, input_units):
         self.n_units = n_units  # number of perceptrons in this layer
         self.input_units = input_units  # number of perceptrons in previous layer
+
         # create an initial bias vector of zeros
         self.bias_vector = np.ones((1, n_units))
         print("bias vector: ")
         print(self.bias_vector)
+
         # create initial weight matrix with random values
         self.weight_matrix = np.random.random((input_units, n_units))
         print("weight matrix: ")
         print(self.weight_matrix)
+
         # put biases into the weight matrix as the last column
         self.weights_and_biases = np.append(self.weight_matrix, self.bias_vector, axis=0)
         print("weights and biases")
         print(self.weights_and_biases)
+
         # create uninitialized pre-activation, activation and input vectors
         self.pre_activation = np.empty((1, self.n_units))
         self.activation = np.empty((1, self.n_units))
         self.input_vector = np.empty((1, self.input_units))
+
+    def __call__(self, x: np.ndarray):
+        self.input_vector = x
+        self.forward_step()
 
     def forward_step(self):
         self.pre_activation = self.input_vector @ self.weights_and_biases
@@ -32,6 +41,10 @@ class Layer:
         self.activation = relu(self.pre_activation)
         print("activation:")
         print(self.activation)
+
+    def backward_step(self):
+        bvg = bias_vector_gradient()  # d_relu * d_L/d_act
+        weight_gradient = self.input_vector @ bvg
 
 
 layer = Layer(3, 2)
---
+++ Week 2/MultiLayerPerceptron.py
@@ -0,0 +1,19 @@
+from Layer import Layer
+
+
+class MultiLayerPerceptron:
+
+    def __init__(self):
+        self.hidden_layer = Layer(10, 1)
+        self.output_layer = Layer(1, 10)
+
+    def forward_step(self, x):
+        self.hidden_layer.input_vector = [[x]]
+        self.hidden_layer.forward_step()
+
+        self.output_layer.input_vector = self.hidden_layer.activation
+        self.output_layer.forward_step()
+
+    def backpropagation(self):
+        return
+# bla
